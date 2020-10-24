1b328e67b47f3f0b8980802411ca5d2a44026976
huzaifa aslam <huzaifaslam45@gmail.com>
2020-10-24 12:34:01 +0500

    added code for phone auth

--- app.js
+++ app.js
@@ -9,23 +9,7 @@ function render(){
 
 function firebaseAuth(){
     var phoneNumber=document.getElementById("number").value
-    // var verCode=document.getElementById("code").value
-    // var signBtn=document.getElementById("sign-in-button")
-    // console.log(signBtn)
-    // firebase.auth().languageCode = 'it';
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
-
-// window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(signBtn, {
-//     'size': 'invisible',
-//     'callback': function(response) {
-//       // reCAPTCHA solved, allow signInWithPhoneNumber.
-//       onSignInSubmit();
-//       console.log("response",response)
-//     }
-//   });
-
-//   window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+console.log("phoneNumber",phoneNumber)
   var appVerifier = window.recaptchaVerifier;
 firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
     .then(function (confirmationResult) {
@@ -34,7 +18,7 @@ firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
       window.confirmationResult = confirmationResult;
       codeResult= window.confirmationResult 
       console.log("codeResult",codeResult)
-
+        alert("message send")
     }).catch(function (error) {
       // Error; SMS not sent
       // ...
