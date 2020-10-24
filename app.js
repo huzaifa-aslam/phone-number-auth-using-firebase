64b6319aad442a48bf9d6d96363c3147cf37648f
huzaifa aslam <huzaifaslam45@gmail.com>
2020-10-24 12:25:27 +0500

    added code for phone auth

--- app.js
+++ app.js
@@ -9,36 +9,41 @@ function render(){
 
 function firebaseAuth(){
     var phoneNumber=document.getElementById("number").value
-    var verCode=document.getElementById("code").value
-    var signBtn=document.getElementById("sign-in-button")
-    console.log(signBtn)
-    firebase.auth().languageCode = 'it';
+    // var verCode=document.getElementById("code").value
+    // var signBtn=document.getElementById("sign-in-button")
+    // console.log(signBtn)
+    // firebase.auth().languageCode = 'it';
 // To apply the default browser preference instead of explicitly setting it.
 // firebase.auth().useDeviceLanguage();
 
-window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(signBtn, {
-    'size': 'invisible',
-    'callback': function(response) {
-      // reCAPTCHA solved, allow signInWithPhoneNumber.
-      onSignInSubmit();
-      console.log("response",response)
-    }
-  });
+// window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(signBtn, {
+//     'size': 'invisible',
+//     'callback': function(response) {
+//       // reCAPTCHA solved, allow signInWithPhoneNumber.
+//       onSignInSubmit();
+//       console.log("response",response)
+//     }
+//   });
 
-  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+//   window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
   var appVerifier = window.recaptchaVerifier;
 firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
     .then(function (confirmationResult) {
       // SMS sent. Prompt user to type the code from the message, then sign the
       // user in with confirmationResult.confirm(code).
       window.confirmationResult = confirmationResult;
-      console.log("confirmationResult",confirmationResult)
+      codeResult= window.confirmationResult 
+      console.log("codeResult",codeResult)
 
     }).catch(function (error) {
       // Error; SMS not sent
       // ...
       console.log("error",error)
     });
+
+}
+
+function codeAuth(){
 
     confirmationResult.confirm(verCode).then(function (result) {
         // User signed in successfully.
